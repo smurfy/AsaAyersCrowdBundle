7fb482582bff810c2e3a8b49f8eb9251e5da2958
smurfy <github@smurfy.de>
2012-04-12 18:08:37 +0200

    Modified The Bundle a lot and removed not needed files

--- AsaAyers/CrowdBundle/retab.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# I know many people say that spaces should be used for indentaion.
-# I disagree, if tabs are used I can use a width of 4, but I see
-# other people like 2 spaces. I've used editors that default to 8.
-# With spaces only one of these can be accomodated, with tabs you
-# can change your own settings and use any width you like.
-find * -type f -iname '*.php' -exec vim -s retab.input {} \;

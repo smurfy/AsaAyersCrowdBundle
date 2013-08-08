f6aaff8da101a6cdf8f381e15a1e2a878ef6d93b
Philipp Andreas <github@smurfy.de>
2013-08-08 16:29:08 +0200

    changes for sf 2.3 support

--- AsaAyersCrowdBundle.php
+++ AsaAyersCrowdBundle.php
@@ -2,8 +2,19 @@
 
 namespace AsaAyers\CrowdBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use AsaAyers\CrowdBundle\DependencyInjection\Security\Factory\SsoFactory;
+use AsaAyers\CrowdBundle\DependencyInjection\Security\Factory\CrowdFactory;
 
 class AsaAyersCrowdBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new SsoFactory());
+        $extension->addSecurityListenerFactory(new CrowdFactory());
+    }
 }
